f6c9b4a45980721d384ccdeb790196a93b3c0a3c
Radhika Ajabe <radhikaajabe072@gmail.com>
2024-04-23 16:22:28 +0530

    Used react router dom to nevigate to specific category by clicking on category name

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+BrowserRouter as Router,
+Routes,
+Route 
+}
+ from 'react-router-dom'
+
 
 export default class App extends Component {  //type rcc and enter for template code of class based component
   render() {
     return (
-      <div>
-      <Navbar/>
-       <News/>
-
-      </div>
+      <>     {/* previously I used div here but got error.therefore router should be root component */}
+        <Router>   
+        <Navbar/>
+          <Routes>
+               <Route exact path="/" element={<News key="general" country="us" pageSize={3} category="general" />}></Route>
+               <Route exact path="/bussiness" element={<News key="bussiness" country="us" pageSize={3} category="bussiness" />}></Route>
+               <Route exact path="/entertainment" element={<News key="entertainment" country="us" pageSize={3} category="entertainment" />}></Route>
+               <Route exact path="/general" element={<News key="general" country="us" pageSize={3} category="general" />}></Route>
+               <Route exact path="/health" element={<News key="health" country="us" pageSize={3} category="health" />}></Route>
+               <Route exact path="/science" element={<News key="science" country="us" pageSize={3} category="science" />}></Route>
+               <Route exact path="/technology" element={<News key="technology" country="us" pageSize={3} category="technology" />}></Route>
+               <Route exact path="/sports" key="sports" element={<News key="sports" country="us" pageSize={3} category="sports" />}></Route>
+              {/** if key is not used to open diffrent category we need to first reload.by using key no need to overload browser ,adds of that category will get opened after clicking on it directly */}
+          </Routes>
+       </Router>
+      </>
     )
   }
 }
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,51 +1,71 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem';
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
 
+  static defaultProps={
+      country:'in',
+      pageSize:4,
+      category:"science"
+  }
+  static propTypes={
+      country:PropTypes.string,
+      pageSize:PropTypes.number,
+      category:PropTypes.string
+  }
+
     constructor(){                                    //constructor of News class
        console.log("I am constructor of News component")
-       super();             //this is necessary otherwise will get this error "'this' is not allowed before 'super()' "
+       super();             //this is necessary otherwise we will get this error "'this' is not allowed before 'super()' "
        this.state={                   //way of using state in class based components like useStates in function based components
           articles:[] , //empty array                             
            page:1,
-           //totalResults:
+           loading:false
        }
     }
 async componentDidMount(){   //lifecycle method that gets called after a component has been rendered to the DOM
-    let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6865fe494053414d9d32407174a03466&page=1&pageSize=20"
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6865fe494053414d9d32407174a03466&page=1&pageSize=${this.props.pageSize}`
+    this.setState({loading:true})
     let data=await fetch(url)        //fetches data from that url
     let parsedData=await data.json();    //converts into json format
-    //console.log(parsedData)
+    console.log(parsedData)
     this.setState({
       articles:parsedData.articles,     //updates data from that url to this articles array
-      totalResults:parsedData.totalResults      //totalResults is number of total adds present in this api link of newsapp.totalResults is a property of a parsed data
+      totalResults:parsedData.totalResults , //totalResults is number of total adds present in this api link of newsapp.totalResults is a property of a parsed data
+      loading:false    
     })   
 }
 //parsedData.articles    articals property of parsedData object
 
 handlePrevClick=async()=>{
-  let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6865fe494053414d9d32407174a03466&page=${this.state.page-1}&pageSize=20`   //pageSize means how many adds should be present in one page
+  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6865fe494053414d9d32407174a03466&page=${this.state.page-1}&pageSize=${this.props.pageSize}`   //pageSize means how many adds should be present in one page
+  this.setState({loading:true})
   let data=await fetch(url)        //fetches data from that url
   let parsedData=await data.json();    //converts into json format
   //console.log(parsedData)
   this.setState({
     page:this.state.page-1,
-    articles:parsedData.articles
+    articles:parsedData.articles,
+    loading:false 
     })
 }
 
 handleNextClick=async()=>{
-  if(this.state.page+1>(Math.ceil(this.state.totalResults/20))){       //this.state.totalResults/20 it gives a total number of pages
+  if(this.state.page+1>(Math.ceil(this.state.totalResults/this.props.pageSize))){       //this.state.totalResults/20 it gives a total number of pages
 //if page number is greater than required pages nothing will happen after clicking on next
   }else{
-  let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6865fe494053414d9d32407174a03466&page=${this.state.page+1}&pageSize=20`
+  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6865fe494053414d9d32407174a03466&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
+  this.setState({loading:true})
   let data=await fetch(url)        //fetches data from that url
   let parsedData=await data.json();    //converts into json format
   console.log(parsedData)
   this.setState({
     page:this.state.page+1,
-    articles:parsedData.articles
+    articles:parsedData.articles,
+    loading:false 
     }
     )
 }
@@ -55,7 +75,8 @@ handleNextClick=async()=>{
   render() {
     return (
       <div className="container my-3">
-      <h1>NewsMonkey-Top headlines</h1>
+      <h1 className='text-center' style={{margin:"35px"}}>NewsMonkey-Top headlines</h1>
+      {this.state.loading && <Spinner/>   }     {/* if loading is true then only show spinner */}
       <div className="row">
       {this.state.articles.map((element)=>{
               
@@ -69,7 +90,7 @@ handleNextClick=async()=>{
         </div>
         <div className='container d-flex justify-content-between'> 
            <button disabled={this.state.page<=1} onClick={this.handlePrevClick} type="button" className="btn btn-dark">&larr; Previous</button>   
-            <button onClick={this.handleNextClick} type="button" className="btn btn-dark">Next &rarr; </button>
+            <button disabled={this.state.page+1>(Math.ceil(this.state.totalResults/this.props.pageSize))} onClick={this.handleNextClick} type="button" className="btn btn-dark">Next &rarr; </button>
         </div>
       </div>
     )
